bb4f8d3595a7d903e5202a81dad4152eb894d0cb
paultbr <paultheo65@gmail.com>
2015-06-27 12:18:12 -0500

    v1
    v1

--- indexapoderado.php
+++ indexapoderado.php
@@ -11,51 +11,19 @@
      <header>
      	<nav>
      		<ul>
-     			<li><a href="#"><span class=""><i class="icon icon-pencil"></i></span>Reportes</a>
+     			<li><a href="#"><span class=""><i class="icon icon-pencil"></i></span>Registrar</a>
                     <ul>
-                    		<li><a href="elegirAsistenciaAlumno.php">Asistencia Alumno</a></li>
-                              <li><a href="notaAlumno.php">Nota Alumno</a></li>
-                              <li><a href="boletine.php">Boletines y Avisos</a></li>
-                              <li><a href="index.html">Salir</a></li>
-                    </ul></li>
+                    		<li><a href="usuario/administrador.php">Administrador</a></li>
+                    		<li><a href="usuario/docente.php">Docente</a></li>
+                   			<li><a href="usuario/apoderado.php">Apoderado</a></li>
+                    		<li><a href="usuario/auxiliar.php">Auxiliar</a></li>
+                    </ul>
+     			</li>
+     			<li><a href="#"><span class=""><i class="icon icon-users"></i></span>???????</a></li>
+     			<li><a href="#"><span class=""><i class="icon icon-mobile2"></i></span>???????</a></li>
+     			<li><a href="#"><span class=""><i class="icon icon-happy"></i></span>??????</a></li>
      		</ul>
      	</nav>
      </header>
-     
-     <div id="tooplate_fecha">
-          <script>
-               var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
-               var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
-               var f=new Date();
-               document.write(diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear());
-          </script>
-     </div>
-
-     <div id="tooplate_hora">
-           <script type="text/javascript">
-               function startTime(){
-               today=new Date();
-               h=today.getHours();
-               m=today.getMinutes();
-               s=today.getSeconds();
-               m=checkTime(m);
-               s=checkTime(s);
-               document.getElementById('reloj').innerHTML=h+":"+m+":"+s;
-               t=setTimeout('startTime()',500);}
-               function checkTime(i)
-               {if (i<10) {i="0" + i;}return i;}
-               window.onload=function(){startTime();}
-           </script>
-           <div id="reloj" style="font-size:20px;">Tiempo: </div>
-     </div>
-
-     <div><h6>BIENVENIDO!</h6></div>
-
-     <div id="mid_title">
-            Usuario : 
-            <?php
-            session_start();
-            $usuario = $_SESSION['usuario'];
-            echo  " $usuario" ?>
-     </div>
+    
 </body>
---
+++ validar3.php
@@ -0,0 +1,63 @@
+<?php
+
+
+require("conexionBD.php");
+
+$username=$_POST['usuario'];
+//	$username=$_POST['mail'];
+$pass=$_POST['pass1'];
+
+
+$sql1=mysql_query("SELECT * FROM tusuario INNER JOIN tadministrador ON tusuario.idusuario = tadministrador.idusuario");
+/*AUXILIAR*/
+ $sql2=mysql_query("SELECT * FROM tusuario INNER JOIN tauxiliar ON tusuario.idusuario = tauxiliar.idusuario");
+ /*APODERADO*/
+ $sql3=mysql_query("SELECT * FROM tusuario INNER JOIN tapoderado ON tusuario.idusuario = tapoderado.idusuario");
+ /*DOCENTE*/
+ $sql4=mysql_query("SELECT * FROM tusuario INNER JOIN tdocente ON tusuario.idusuario = tdocente.idusuario");
+ /*------*/
+ $f1=mysql_fetch_array($sql1);
+ $f2=mysql_fetch_array($sql2);
+ $f3=mysql_fetch_array($sql3);
+ $f4=mysql_fetch_array($sql4);
+
+ /*CONSULTAS*/
+    if($username==$f1['idadministrador']){
+	        if($pass==$f1['password']){
+		        echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
+		        echo "<script>location.href='indexadministrador.php'</script>";
+	        }
+    }
+    else{
+    	if ($username==$f2['idauxiliar']) {
+    		if ($pass==$f2['password']) {
+    			echo '<script>alert("BIENVENIDO AUXILIAR")</script> ';
+		        echo "<script>location.href='indexauxiliar.php'</script>";
+    		}
+    	}
+    	else{
+            if ($username==$f3['idapoderado']) {
+	    		if ($pass==$f3['password']) {
+	    			echo '<script>alert("BIENVENIDO PADRE DE FAMILIA")</script> ';
+			        echo "<script>location.href='indexapoderado.php'</script>";
+	    		}
+    	    }
+    	    else{
+    	    	if ($username==$f4['iddocente']) {
+		    		if ($pass==$f4['password']) {
+		    			echo '<script>alert("BIENVENIDO DOCENTE")</script> ';
+				        echo "<script>location.href='indexdocente.php'</script>";
+		    		}
+    	        }
+    	        else
+    	        {
+
+				    echo '<script>alert("ESTE USUARIO NO EXISTE, PORFAVOR REGISTRESE PARA PODER INGRESAR")</script> ';
+				    echo "<script>location.href='indexx.php'</script>";
+    	        }
+    	    }
+
+    	}
+    } 
+
+?>
